d748d771e9f59208400de67aa4bcb5b5fd69c7ac
vipulanand123 <mailtovipulanand@gmail.com>
2024-05-14 17:03:46 +0530

    Update:Expiry File automated

---
+++ update_expiry.bat
@@ -0,0 +1,3 @@
+cd C:\Users\vipulanand\Documents\iv_charts\new_iv_charts\data_analytics
+C:\Users\vipulanand\Documents\iv_charts\new_iv_charts\data_analytics\venv\Scripts\python.exe update_expiry.py
+pause
